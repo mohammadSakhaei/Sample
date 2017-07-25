ceb95de3716278487c5e6c82b2817b4e0dc808f7
IT Staff <IT Staff@CLIENT-2>
2017-07-25 10:42:38 +0430

    Add Scripts and other

--- SimpleLogin/SimpleLogin/Controllers/HomeController.cs
+++ SimpleLogin/SimpleLogin/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SimpleLogin.Models;
 
 namespace SimpleLogin.Controllers
 {
@@ -13,23 +14,31 @@ namespace SimpleLogin.Controllers
             return View();
         }
 
-        public IActionResult About()
+        //Note : you can bind same list from database  
+        List<City> objList = new List<City>()
         {
-            ViewData["Message"] = "Your application description page.";
 
-            return View();
+            new City {Id=1,Name="Latur" },
+            new City {Id=2,Name="Mumbai" },
+            new City {Id=3,Name="Pune" },
+            new City {Id=4,Name="Delhi" },
+            new City {Id=5,Name="Dehradun" },
+            new City {Id=6,Name="Noida" },
+            new City {Id=7,Name="New Delhi" }
+
+        };
+
+        [HttpPost]
+        public JsonResult Index(string prefix)
+        {
+            
+
+            //Searching records from list using LINQ query  
+            var cityName = objList.Where(o => o.Name.Contains(prefix)).Select(o => o.Name);
+
+            //return Json(CityName, JsonRequestBehavior.AllowGet);
+            return Json(cityName);
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
     }
 }
